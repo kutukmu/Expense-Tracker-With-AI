bad0fc152a1dbc97fb09ea2b7a4c7aa24725d25c
kutukmu <kutukmu@gmail.com>
2025-09-28 11:15:18 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="bg-red-500">
+      Hello World Again, This time I will be a real developer with my problem
+      solving abilities
+    </div>
+  );
+}
