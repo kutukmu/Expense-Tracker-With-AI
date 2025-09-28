5a82782463b83bec5f38e525930f908dbe266ed1
kutukmu <kutukmu@gmail.com>
2025-09-28 16:36:38 -0500

    Guest Page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "@/components/Guest";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest></Guest>;
+  }
   return (
     <div className="bg-red-500">
       Hello World Again, This time I will be a real developer with my problem
